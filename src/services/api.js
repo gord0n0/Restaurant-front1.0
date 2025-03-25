f852a48a3fa37a114a18f0e48854fa68eeb2fd21
gord0n0 <itsgordon4@gmail.com>
2025-03-25 12:36:53 +0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import router from '../router/index';
 import TokenService from './token.service';
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    baseURL: "https://restaurant-back1-0.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
